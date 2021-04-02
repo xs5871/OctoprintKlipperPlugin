51ca8dc259a4ae817825b41b3310bec127411c7f
thelastWallE <12502210+thelastWallE@users.noreply.github.com>
2021-04-03 00:49:40 +0200

    fix for python2.7

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ plugin_package = "octoprint_klipper"
 
 plugin_name = "OctoKlipper"
 
-plugin_version = "0.3.6"
+plugin_version = "0.3.6a"
 
 plugin_description = """A plugin for OctoPrint to configure,control and monitor the Klipper 3D printer software."""
 
